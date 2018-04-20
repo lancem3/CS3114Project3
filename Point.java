73c278635286876ab14bac59f346c555a35e1be3
lancem3 <lancem3@vt.edu>
2018-04-20 11:06:51 -0400

    Update Point.java

--- Point.java
+++ Point.java
@@ -1,9 +1,26 @@
 public class Point {
     int x, y, z;
-
+    
+    public Point() {
+        
+    }
     public Point(int x, int y, int z) {
         this.x = x;
         this.y = y;
         this.z = z;
+    }   
+    public void setX(int xp)
+    {
+        x = xp
+    }
+        
+    public void setY(int yp)
+    {
+        y = yp;
+    }
+ 
+    public void setZ(int zp)
+    {
+        z = zp;
     }
 }
